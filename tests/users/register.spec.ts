9647704ddaa7ccac6dec03336fce7bfe4ce0a76d
rakshith_s <rakshithsantosh@gmail.com>
2025-07-22 20:49:28 +0530

    test suit for valid json

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -21,6 +21,26 @@ describe('POST /auth/register', () => {
             //Assert
             expect(response.statusCode).toBe(201)
         })
+
+        it('should return a valid json', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'Rakshith',
+                lastName: 'S',
+                email: 'rakshith@mern.space',
+                password: 'password123',
+            }
+
+            //Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            //Assert
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'))
+        })
     })
     describe('Fields are missing', () => {})
 })
