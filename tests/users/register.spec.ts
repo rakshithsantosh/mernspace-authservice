8fc1eb1628d42265f15d10cb305967fd612b2de5
rakshith_s <rakshithsantosh@gmail.com>
2025-07-24 11:01:32 +0530

    add typeorm

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -41,6 +41,23 @@ describe('POST /auth/register', () => {
                 (response.headers as Record<string, string>)['content-type'],
             ).toEqual(expect.stringContaining('json'))
         })
+
+        // it('should persist the user in database ', async () => {
+        //     //Arrange
+        //     const userData = {
+        //         firstName: 'Rakshith',
+        //         lastName: 'S',
+        //         email: 'rakshith@mern.space',
+        //         password: 'password123',
+        //     }
+
+        //     //Act
+        //     const response = await request(app)
+        //         .post('/auth/register')
+        //         .send(userData)
+
+        //     //Assert
+        // })
     })
     describe('Fields are missing', () => {})
 })
