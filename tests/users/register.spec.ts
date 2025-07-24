1ddc3ad894b45d036e8f426fe8d72306e2ccc9ac
rakshith_s <rakshithsantosh@gmail.com>
2025-07-24 15:08:03 +0530

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,27 @@
 import request from 'supertest'
 import app from '../../src/app'
+import { DataSource } from 'typeorm'
+import { AppDataSource } from '../../src/config/data-source'
+import { truncateTables } from '../utils'
+import { User } from '../../src/entity/User'
 
 describe('POST /auth/register', () => {
+    let connection: DataSource
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    //teardown after all tests are done basically cleanup after all tests
+    beforeEach(async () => {
+        //database truncate
+        //this will remove all the data from the database
+        await truncateTables(connection)
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             //AAA
@@ -42,22 +62,30 @@ describe('POST /auth/register', () => {
             ).toEqual(expect.stringContaining('json'))
         })
 
-        // it('should persist the user in database ', async () => {
-        //     //Arrange
-        //     const userData = {
-        //         firstName: 'Rakshith',
-        //         lastName: 'S',
-        //         email: 'rakshith@mern.space',
-        //         password: 'password123',
-        //     }
+        it('should persist the user in database ', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'Rakshith',
+                lastName: 'S',
+                email: 'rakshith@mern.space',
+                password: 'password123',
+            }
 
-        //     //Act
-        //     const response = await request(app)
-        //         .post('/auth/register')
-        //         .send(userData)
+            //Act
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
 
-        //     //Assert
-        // })
+            //Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+
+            expect(users).toHaveLength(1)
+            expect(users[0].firstName).toBe(userData.firstName)
+            expect(users[0].lastName).toBe(userData.lastName)
+            expect(users[0].email).toBe(userData.email)
+        })
     })
     describe('Fields are missing', () => {})
 })
